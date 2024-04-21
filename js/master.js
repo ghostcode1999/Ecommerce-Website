b8facd6aafd5897d3f55ff3bf555674aab391505
ghostcode <ghostcode1999@gmail.com>
2024-04-21 12:06:51 +0100

    Add Ecommerce website files

---
+++ js/master.js
@@ -0,0 +1,14 @@
+// jQuery Based Javascript
+// -------------------------
+//
+/*******************************************
+ * Template Name: Sass Architecture
+ * Updated: Nov 12 2023 with Bootstrap v5.3.1
+ * Author: Ghostcode
+ * PSD Designer: GhostCode
+ *******************************************/
+//
+$(function () {
+	
+	
+});
